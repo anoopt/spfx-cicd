bca1a7904fdb8b1b7e47becba5236f972d840bad
anoopt <anoop@live.co.uk>
2019-08-20 09:34:58 +0100

    Testing CI

--- src/webparts/helloWorld/HelloWorldWebPart.ts
+++ src/webparts/helloWorld/HelloWorldWebPart.ts
@@ -22,7 +22,7 @@ export default class HelloWorldWebPart extends BaseClientSideWebPart<IHelloWorld
           <div class="${ styles.row }">
             <div class="${ styles.column }">
               <span class="${ styles.title }">Welcome to SharePoint!</span>
-              <p class="${ styles.subTitle }">Customize SharePoint experiences using Web Parts.</p>
+              <p class="${ styles.subTitle }">Customize SharePoint experiences using Web Parts</p>
               <p class="${ styles.description }">${escape(this.properties.description)}</p>
               <a href="https://aka.ms/spfx" class="${ styles.button }">
                 <span class="${ styles.label }">Learn more</span>
